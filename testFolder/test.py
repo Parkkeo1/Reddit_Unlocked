6e91741492ffa07828197a33ec10606d4757868f
Benjamin Vacek <ben.vacek@gmail.com>
2017-09-25 17:51:54 -0500

    Added a reddit scraper to test.py it asks for a subreddit, pulls the first twenty posts and displays a dictionary of their domain name along with their score. Feel free to exchange the 'domain' and 'score' with other data points.

--- testFolder/test.py
+++ testFolder/test.py
@@ -1,1 +1,17 @@
-print("Hello, world!")
+from bs4 import BeautifulSoup
+import json
+import requests
+
+subreddit = input('Insert subreddit: ' )
+
+site = requests.get('https://reddit.com/r/{}.json'.format(subreddit), headers={'user-agent': 'Mozilla/5.0'}
+)
+
+reddit_json = json.loads(site.text)
+
+master_dict = {}
+
+for i in range(0, 19):	
+	master_dict[reddit_json['data']['children'][i]['data']['domain']] = reddit_json['data']['children'][i]['data']['score']
+
+print(master_dict)
