ebbfd30c8efe46bff90154a1638b0235b8c31b6a
Gary Braznichenko <garyab2@illinois.edu>
2017-12-03 23:50:42 -0600

    function created for graphing that technically works

--- garyab2/dict_to_graph.py
+++ garyab2/dict_to_graph.py
@@ -6,34 +6,59 @@ from plotly.graph_objs import *
 import pandas as pd
 
 
-#takes a list of dictionaries of keywords from body text as input and returns the url for the plotly html embedding of
+#takes a dictionary of dictionaries of keywords from body text as input and returns the url for the plotly html embedding of
 #scatterplot made from the keywords and their attributes
-def body_to_graph(posts = [], subreddit):
+#'Keyword','Occurences', 'Upvotes', 'Downvotes',  "Score", "Subjectivity", "Polarity", "Domain"
+def body_to_graph(words = {}, subreddit = str):
     """
     :type subreddit: String
     """
-    data_df = pd.DataFrame(posts)
+    frames = []
+    #Turns dictionary of dictionaries into list of dataframes
+    for key, value in words.items():
+        frames.append(pd.DataFrame(data = value, columns = [key], index = ['Keyword','Occurences', 'Upvotes', 'Downvotes', 'Score', 'Subjectivity', 'Polarity', 'Domain']).transpose())
+    #Concatenates the list of dataframes
+    data_df = pd.concat(frames)
     trace1 = go.Scatter(
-        x = data_df.month_posted,
-        y = data_df.score,
+        y = data_df.Subjectivity, #Subjectivity of the text the keyword was found in on y axis
+        x = data_df.Occurences * data_df.Score,#Occurrences * Score on x-axis for more spread out data
         mode = 'markers',
         marker = dict(
-            size = (data_df.upvotes + data_df.downvotes) / 200000 * 50,
-            color = data_df.polarity,
+            size =  (data_df.Occurences) * 20, #Occurrences of Keyword for size
+            color = data_df.Polarity, #Polarity for color of the post (blue is sad, red is happy)
             colorscale = 'Portland',
             showscale = True
         ),
-        text = data_df.keyword + '\n' + data_df.domain
+        text = "Keyword: " + data_df.Keyword
     )
     layout = go.Layout(
-        title = 'Stats of top reddit/r/' + subreddit + ' posts',
-        xaxis = dict(
-            title = 'month_posted',
-            ticks = 12,
-        ),
+        annotations=Annotations([
+            Annotation(
+                x=0.5004254919715793,
+                y=-0.16191064079952971,
+                showarrow=False,
+                text='Custom x-axis title',
+                xref='paper',
+                yref='paper'
+            ),
+            Annotation(
+                x=-0.04944728761514841,
+                y=0.4714285714285711,
+                showarrow=False,
+                text='Custom y-axis title',
+                textangle=-90,
+                xref='paper',
+                yref='paper'
+            )
+        ]),
+        title = 'Stats of top reddit/r/' + subreddit + ' keywords',
         yaxis = dict(
-            title = 'score',
-            ticklen = 5,
+            title = 'Subjectivity',
+            ticks = 5,
+        ),
+        xaxis = dict(
+            title = 'popularity',
+            ticklen = 10,
         )
     )
     data = [trace1]
